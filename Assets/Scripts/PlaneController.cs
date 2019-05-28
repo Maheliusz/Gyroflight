fd6d64afba17061a1844483860a05875d7e34f9d
Michał Zakrzewski <miczak96@gmail.com>
2019-05-28 15:48:40 +0200

    Working on game field restriction

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -6,15 +6,38 @@ public class PlaneController : MonoBehaviour
 {
     public float score = 0f;
     public float health = 100f;
+    //public float speed = 100f;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position += Vector3.forward * Time.deltaTime; //TODO: put real movement here
+        //this.transform.position += Vector3.forward * Time.deltaTime; //TODO: put real movement here
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.position += Vector3.left * speed * Time.deltaTime;
+            rb.MovePosition(Vector3.left * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.position += Vector3.right * speed * Time.deltaTime;
+            rb.MovePosition(Vector3.right * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //transform.position += Vector3.up * speed * Time.deltaTime;
+            rb.MovePosition(Vector3.up * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //transform.position += Vector3.down * speed * Time.deltaTime;
+            rb.MovePosition(Vector3.down * Time.deltaTime);
+        }
     }
 
     public void DealDamage(float amount)
